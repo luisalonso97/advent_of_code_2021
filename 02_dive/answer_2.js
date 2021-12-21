40a96e31c2130385e81c37f2edc1608f674ffb01
Luis Alonso <luis.a.alonso@oracle.com>
2021-12-20 22:42:19 -0600

    Added answer_2 for day 02

--- 02_dive/answer_2.js
+++ 02_dive/answer_2.js
@@ -7,13 +7,37 @@ const MOVEMENTS = {
   'UP'     : /^up\s+(?<scalar>\d+)$/
 };
 
-const filter_raw_movement = (raw_movements, type) => {
-  return R.filter(line => MOVEMENTS[type].test(line), raw_movements);
-}
-
 const get_movement_scalar = (type, line) => {
   const match = line.match(MOVEMENTS[type]);
   return parseInt(match.groups.scalar);
+}
+const get_movement = raw_movement => {
+  if (MOVEMENTS['FORWARD'].test(raw_movement))
+    return { type: 'FORWARD', scalar: get_movement_scalar('FORWARD', raw_movement) };
+  else if (MOVEMENTS['UP'].test(raw_movement))
+    return { type: 'UP', scalar: get_movement_scalar('UP', raw_movement) };
+  else if (MOVEMENTS['DOWN'].test(raw_movement))
+    return { type: 'DOWN', scalar: get_movement_scalar('DOWN', raw_movement) };
+}
+
+const calculate_answer = raw_movements => {
+  let x_scalar = 0;
+  let y_scalar = 0;
+  let aim = 0;
+
+  R.forEach(
+    raw_mov => {
+      const {type, scalar} = get_movement(raw_mov);
+      if      (type == 'UP')   aim -= scalar;
+      else if (type == 'DOWN') aim += scalar;
+      else if (type == 'FORWARD') {
+        x_scalar += scalar;
+        y_scalar += scalar * aim;
+      }
+    }, raw_movements
+  );
+
+  return x_scalar * y_scalar;
 }
 
 const main = () => {
